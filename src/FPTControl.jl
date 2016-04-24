f0aea48ac75059fd4196c607f66109d237db9db6
Krisztián Kósi <krisztian.kosi@gmail.com>
2016-04-24 21:20:42 +0200

    first version

--- src/FPTControl.jl
+++ src/FPTControl.jl
@@ -4,16 +4,41 @@ module FPTControl
 # Fixed Point Transformation Control toolbox #
 ##################################
 
+# Global variable definitions for using Fixed Point Transformation
+global Λ
+global Kc
+global Bc
+global Ac
+global Adaptive
+global δt
+global LONG
+
+
+Λ=6
+Kc=1e6
+Bc=1
+Ac=1e-5
+Adaptive=1
+δt=1e-3
+LONG=Int(2e5)
+
+
+
 # package code goes here
-function KB(n,Λ,errors,nominal)
+function KB(n,errors,nominal)
   out=0
   #println(2)
   for l=0:n
     out=out+(factorial(n)/(factorial(l)*factorial(n-l))*errors[l+1]*Λ^(n-l))
-    println(factorial(n-l))
+    #println(factorial(n-l))
   end
   out=out+nominal
   return out
+end
+
+function Integ(Integral,input)
+    out=Integral+δt*input
+    return out
 end
 
 function sigmoid(x)
@@ -22,7 +47,7 @@ function sigmoid(x)
 end
 
 function G_SISO(past_input,past_response,desired)
-  out=(K+past_input)*(1 + B * tanh(A * (past_response - desired))) - K
+  out=(Kc+past_input)*(1 + Bc * tanh(Ac * (past_response - desired))) - Kc
   return out
 end
 function G_MIMO(past_input,past_response,desired) # Inputs and Outputs are vectors
@@ -32,12 +57,14 @@ function G_MIMO(past_input,past_response,desired) # Inputs and Outputs are vecto
 	# (it is not near the Fixed Point)
   if error_norm>error_limit
     e_direction=(past_response-desired)/error_norm
-    B_factor= B * sigmoid(A *error_norm)
-    out=(1+B_factor)*past_input +B_factor*K*e_direction
+    B_factor= Bc* sigmoid(Ac *error_norm)
+    out=(1+B_factor)*past_input +B_factor*Kc*e_direction
   else
     out=past_input # Almost in the Fixed Point
   end
   return out
 end
 
+
+export Λ,Kc,Bc,Ac,Adaptive,δt,LONG,KB,Integ,G_SISO,G_MIMO
 end # module
