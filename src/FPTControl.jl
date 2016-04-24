feeda3e5e90353f2beaede37782bc3a0e07b3c53
Krisztián Kósi <krisztian.kosi@gmail.com>
2016-04-25 00:55:41 +0200

    v0.0.1

--- src/FPTControl.jl
+++ src/FPTControl.jl
@@ -4,40 +4,24 @@ module FPTControl
 # Fixed Point Transformation Control toolbox #
 ##################################
 
-# Global variable definitions for using Fixed Point Transformation
-global Λ
-global Kc
-global Bc
-global Ac
-global Adaptive
-global δt
-global LONG
 
-
-Λ=6
-Kc=1e6
-Bc=1
-Ac=1e-5
-Adaptive=1
-δt=1e-3
-LONG=Int(2e5)
 
 
 
 # package code goes here
-function KB(n,errors,nominal)
+function KB(n,lambda,errors,nominal)
   out=0
   #println(2)
   for l=0:n
-    out=out+(factorial(n)/(factorial(l)*factorial(n-l))*errors[l+1]*Λ^(n-l))
+    out=out+(factorial(n)/(factorial(l)*factorial(n-l))*errors[l+1]*lambda^(n-l))
     #println(factorial(n-l))
   end
   out=out+nominal
   return out
 end
 
-function Integ(Integral,input)
-    out=Integral+δt*input
+function Integ(Integral,step,input)
+    out=Integral+step*input
     return out
 end
 
@@ -46,11 +30,11 @@ function sigmoid(x)
   return s
 end
 
-function G_SISO(past_input,past_response,desired)
+function G_SISO(past_input,past_response,desired,Kc,Bc,Ac)
   out=(Kc+past_input)*(1 + Bc * tanh(Ac * (past_response - desired))) - Kc
   return out
 end
-function G_MIMO(past_input,past_response,desired) # Inputs and Outputs are vectors
+function G_MIMO(past_input,past_response,desired,Kc,Bc,Ac) # Inputs and Outputs are vectors
 #  need control parameters  K B A
 	error_norm=vecnorm(past_response - desired,2)
 	# If the norm of the error is greater then the limnit compute the deformation
@@ -66,5 +50,5 @@ function G_MIMO(past_input,past_response,desired) # Inputs and Outputs are vecto
 end
 
 
-export Λ,Kc,Bc,Ac,Adaptive,δt,LONG,KB,Integ,G_SISO,G_MIMO
+export KB,Integ,G_SISO,G_MIMO
 end # module
