d2800468f9e9e79104503e0b21f5a13a5ab4761b
Krisztián Kósi <krisztian.kosi@gmail.com>
2016-04-27 08:44:34 +0200

    some additions

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ Fixed Point Transformation Based Control
 
 Some examples can be found on my Control github repo.
 https://github.com/krisztiankosi/Control
+
+The Original article for this method is in [Acta Polytechnica Hungarica](http://uni-obuda.hu/journal) published by [Óbuda University](https://www.uni-obuda.hu)
+It is an Open Access Journal, IF for 2014: 0.649
+[Robust Fixed Point Transformations in Adaptive Control Using Local Basin of Attraction](http://uni-obuda.hu/journal/Tar_Bito_Nadai_Machado_17.pdf)
+
+
+##Usage examples
+
+If we see a 2nd order system,
+
+```KB(n,lambda,errors,nominal)```
+compute the
+$$ (\frac{d}{dt}+\Lambda)^{n+1}error_integral$$
--- src/FPTControl.jl
+++ src/FPTControl.jl
@@ -4,36 +4,39 @@ module FPTControl
 # Fixed Point Transformation Control toolbox #
 ##################################
 
-
-
-
-
-# package code goes here
+# In PID case n=the order of the system +1
+# In PD case n=the order of the system
+# lamda is a control parameter for tuning the PID or PD
+# errors is a vector where you put togather the related errors.
+# example for PID n=3 (2nd order system) errors=[error_int,error,error_p], where error=qNominal - q (by defininition nominal Trajectory - Realized Trajectory)
+# errror_p is the time derivative of the error
+# for PD ,when n=2 (2nd order system) errors=[error,error_p]
 function KB(n,lambda,errors,nominal)
   out=0
-  #println(2)
-  for l=0:n
+  for l=0:n-1
     out=out+(factorial(n)/(factorial(l)*factorial(n-l))*errors[l+1]*lambda^(n-l))
-    #println(factorial(n-l))
   end
   out=out+nominal
   return out
 end
-
+# Simple Euler integral (rectangle method)
 function Integ(Integral,step,input)
     out=Integral+step*input
     return out
 end
 
+# Simple Sigmoid function, mostly you can use any Sigmoid type function, like tanh()
 function sigmoid(x)
 	s=x/(1+abs(x))
   return s
 end
-
+# The Original Deformation function for SISO case (note here I used tanh() for sigmoid function)
 function G_SISO(past_input,past_response,desired,Kc,Bc,Ac)
   out=(Kc+past_input)*(1 + Bc * tanh(Ac * (past_response - desired))) - Kc
   return out
 end
+
+# The Original Deformation function for MIMO case
 function G_MIMO(past_input,past_response,desired,Kc,Bc,Ac) # Inputs and Outputs are vectors
 #  need control parameters  K B A
 	error_norm=vecnorm(past_response - desired,2)
